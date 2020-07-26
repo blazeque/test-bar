1eddd67b82cfba14c5f4c206afbd10831040a3d9
blazeque <daniel.blazeque@gmail.com>
2020-07-26 19:12:11 +0100

    structure project

---
+++ frontend/src/template/menu.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default props => (
+    <nav className='navbar navbar-inverse bg-inverse'>
+        <div className='container'>
+            <div className='navbar-header'>
+            <ul className="nav navbar-nav">
+                    <li><a href='#/todos'>Kitchen</a></li>
+                </ul>
+            </div>
+
+        </div>
+    </nav>
+)
